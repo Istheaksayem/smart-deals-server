54f3d9eba64978216ec0ec1b8ed7f8814ce6c102
Istheaksayem <istheakahmed9345@gmail.com>
2025-11-01 18:48:30 +0600

    Connect MongoDB and Post API

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
 const express = require('express');
 const cors = require('cors');
-const app =express()
-const port =process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const app = express()
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-  res.send('smart server is running')
+
+const uri = "mongodb+srv://smartdbUser:0rAx4DT7RVOGsvdL@cluster0.ba90y0b.mongodb.net/?appName=Cluster0";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+// 0rAx4DT7RVOGsvdL
+
+app.get('/', (req, res) => {
+    res.send('smart server is running')
 })
 
-app.listen(port, ()=>{
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db('smart_db')
+        const productsCollection = db.collection('products');
+
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result)
+        })
+
+        app.delete('/products/:id', (req, res) => {
+            const id =req.params.id;
+        })
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+app.listen(port, () => {
     console.log(`smart server is running on port ${port}`)
 })
